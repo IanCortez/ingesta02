9be75fd2eed9eb83b4811cc792a5557d2e4f74c2
IanCortez <85956402+IanCortez@users.noreply.github.com>
2025-05-04 22:54:22 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -1,3 +1,22 @@
+import mysql.connector
+
+host_name = "172.31.86.125" # IPv4 privada de "MV Bases de Datos"
+port_number = "8005"
+user_name = "root"
+password_db = "utec"
+database_name = "bd_api_employees"  
+
+
+def get_employees():
+    mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)  
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM employees")
+    result = cursor.fetchall()
+    cursor.close()
+    mydb.close()
+    return {"employees": result}
+
+
 import boto3
 
 ficheroUpload = "data.csv"
