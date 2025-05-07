c049fdf74ee296a0a2c0466814b45c8c7711f1a3
IanCortez <85956402+IanCortez@users.noreply.github.com>
2025-05-07 18:55:55 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -1,4 +1,9 @@
 import mysql.connector
+import boto3
+
+
+ficheroUpload = "data.csv"
+nombreBucket = "gcr-output-01"
 
 host_name = "172.31.86.125" # IPv4 privada de "MV Bases de Datos"
 port_number = "8005"
@@ -8,19 +13,11 @@ database_name = "bd_api_employees"
 
 
 def get_employees():
-    mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)  
-    cursor = mydb.cursor()
-    cursor.execute("SELECT * FROM employees")
-    result = cursor.fetchall()
-    cursor.close()
+    mydb = mysql.connector.connect(host=host_name, port=port_number, user=user_name, password=password_db, database=database_name)
+    df = pd.read_sql("SELECT * FROM employees", mydb)
     mydb.close()
-    return {"employees": result}
-
-
-import boto3
-
-ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+    df.to_csv(ficheroUpload, index=False)
+    
 
 s3 = boto3.client('s3')
 response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
